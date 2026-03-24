a138927b84e6cbdd3e3798080ad1324f2513d602
sergio <sfernandezrodriguez@danielcastelao.org>
2026-03-24 14:09:29 +0100

    Añadiendo todo lo necesario para el examen

--- juego.py
+++ juego.py
@@ -1,25 +1,32 @@
 from tablero import Tablero
-from nave import Nave
+
 class Juego:
     def __init__(self):
-        self.lanzar_ataque(3,2)
+        self.tablero = Tablero()
+        self.lanzar_ataque(1, 0)
+        self.lanzar_ataque(1, 0)
+        self.lanzar_ataque(1, 1)
+        self.lanzar_ataque(1, 2)
+        self.lanzar_ataque(1, 1)
+        self.lanzar_ataque(1, 4)
 
 
-    def lanzar_ataque(self,x,y):
-        print(f"Atacando a {x}, {y}")
-        obj_tablero = Tablero()
-        obj_tablero.comprobar_impacto(x,y)
-
-    def mostrar_resultado(self,resultado):
+    def mostrar_resultado(self, resultado):
+        #dependiendo del resultado que genere da una cosa o otra
         if resultado == 0:
             print("Agua")
         elif resultado == 1:
             print("Tocado")
-
-        else:
+        elif resultado == 2:
             print("Hundido")
 
+    def lanzar_ataque(self, x, y):
+        print(f"Ataque a {x},{y}")
+        casilla = self.tablero.casillero[x][y]
 
+        resultado = casilla.recibir_disparo()
+
+        self.mostrar_resultado(resultado)
 
 if __name__ == "__main__":
     Juego()
--- nave.py
+++ nave.py
@@ -1,19 +1,23 @@
 class Nave:
-    def __init__(self, nombre, tamano):
+    def __init__(self, nombre, tipo, vida):
         self.nombre = nombre
-        self.vida = tamano
+        self.tipo = tipo
+        self.vida = vida
         self.hundido = False
         self.TOCADO = 1
         self.HUNDIDO = 2
 
 
+
     def recibir_disparo(self):
+        if self.hundido:
+            return self.HUNDIDO
+
         self.vida -= 1
         if self.vida <= 0:
             self.hundido = True
+            print(f"{self.nombre} hundido")
             return self.HUNDIDO
-
         else:
-           return self.TOCADO
-
-
+            print(f"{self.nombre} tocado. Vida restante: {self.vida}")
+            return self.TOCADO
